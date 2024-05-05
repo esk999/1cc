8da0992b4ae6dc5ddc30002a7dedd6defcc8db43
esk999 <esk08142003@gmail.com>
2024-05-05 22:36:48 +0900

    コメントブロックの追加

--- tokenize.c
+++ tokenize.c
@@ -115,6 +115,26 @@ void tokenize(char *p){
             continue;
         }
 
+        // 行コメントをスキップ
+        if (strncmp(p, "//", 2) == 0) {
+            p += 2;
+            while (*p != '\n'){
+                p++;
+            }       
+            continue;
+        }
+
+        // ブロックコメントをスキップ
+        if (strncmp(p, "/*", 2) == 0) {
+        char *q = strstr(p + 2, "*/");
+        if (!q){
+            error_at(p, "コメントが閉じられていません");
+        }
+        p = q + 2;
+        continue;
+        }
+
+
         //2文字の比較演算子のときのトークナイズの長さを2にする
         if(startswitch(p, "==") || startswitch(p, "!=") ||
             startswitch(p, "<=") || startswitch(p, ">=")){
